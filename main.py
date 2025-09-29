5648eee8107c2c7809266f49a12e03a4e5eeb3f7
Tcheesy <102567128+Chizobaonorh@users.noreply.github.com>
2025-09-29 19:23:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import cvlib as cv
 from cvlib.object_detection import draw_bbox
 from numpy.lib.polynomial import poly
 
-image = cv2.imread("cars.jpeg")
+image = cv2.imread("./cars.webp")
 box, label, count = cv.detect_common_objects(image)
 output = draw_bbox(image, box, label, count)
 plt.imshow(output)
