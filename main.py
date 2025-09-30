9ad1426d55677862db49fb8f44be35fef896f1d4
Tcheesy <102567128+Chizobaonorh@users.noreply.github.com>
2025-09-30 16:27:48 +0100

    Update main.py using torchvisioon to create this model

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-import cv2
-import numpy as np
+from torchvision.io import decode_image
+from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2, FasterRCNN_ResNet50_FPN_V2_Weights
+from torchvision.utils import draw_bounding_boxes
+from torchvision.transforms.functional import to_pil_image
 import matplotlib.pyplot as plt
-import cvlib as cv
-from cvlib.object_detection import draw_bbox
-from numpy.lib.polynomial import poly
 
-image = cv2.imread("./cars.webp")
-box, label, count = cv.detect_common_objects(image)
-output = draw_bbox(image, box, label, count)
-plt.imshow(output)
+img = decode_image("/content/drive/MyDrive/dataset/cars.webp")
+
+weights = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
+model = fasterrcnn_resnet50_fpn_v2(weights=weights, box_score_thresh=0.9)
+model.eval()
+
+preprocess = weights.transforms()
+batch = [preprocess(img)]
+
+prediction = model(batch)[0]
+labels = [weights.meta["categories"][i] for i in prediction["labels"]]
+box = draw_bounding_boxes(img, boxes=prediction["boxes"],
+                          labels=labels,
+                          colors="red",
+                          width=4,
+                          font_size=30,)
+
+im = to_pil_image(box.detach())
+
+plt.figure(figsize=(12, 10))
+plt.imshow(im)
+plt.axis('off')
+plt.title("Object Detection Results")
 plt.show()
-print("Number of cars in this image are " +str(label.count('car')))
+
